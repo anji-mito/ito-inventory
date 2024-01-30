a9bd21701e7941bb9617b87fc13fa0a2fdc62f28
anji-mito <120057673+anji-mito@users.noreply.github.com>
2024-01-31 00:25:31 +0500

    init

---
+++ src/main/java/com/ito/inventory/controller/ItemController.java
@@ -0,0 +1,11 @@
+package com.ito.inventory.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/items")
+public class ItemController {
+
+}
